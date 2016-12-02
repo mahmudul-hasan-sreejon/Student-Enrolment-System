dd52b82d9b60912f804173605f65b971bb08aebf
sreejonK19 <sreejon07@gmail.com>
2016-12-02 22:34:45 +0600

    enroll updated

--- enroll_name_del_pro.php
+++ enroll_name_del_pro.php
@@ -9,18 +9,18 @@
 	}
 ?>
 
-<?php
-	if( isset($_GET['delete']) ) {
-		$admno = mysql_real_escape_string($_GET['admno']);
-		$result = mysql_query("SELECT * FROM stud_adm WHERE adm_no='$admno'");
-		$row = mysql_fetch_array($result);
-?>
-
 <html>
 	<body>
-
 		<br><br><br><br><br>
 		<center>
+
+			<?php
+				if( isset($_GET['delete']) ) {
+					$admno = mysql_real_escape_string($_GET['admno']);
+					$result = mysql_query("SELECT * FROM stud_adm WHERE adm_no='$admno'");
+					$row = mysql_fetch_array($result);
+			?>
+			
 			<form action="enroll_name_del_confirm.php" method="GET">
 				<table border="20" height="100" cellspacing="5" cellpadding="5" bordercolor='#21DBD9' bgcolor='#E5F4F4'>
 					<input type="text" name="admno" value="<?php echo $row['adm_no'];?>" hidden>
@@ -57,18 +57,20 @@
 					</TR>
 				</table>
 			</form>
+
+			<?php
+				}
+				else {
+					echo "<center><h3>"."No Such Admission found"."</h3></center>";
+					echo '<p><center><input type="button" style="height:30px/;width:200px" value="Retry" onclick="window.location =\'enroll_name_del.php\'" /></center></p>';
+					echo "</br></br>";
+					echo '<p><center><input type="button" style="height:30px/;width:200px" value="Go to Home" onclick="window.location =\'dashboard.php\'" /></center></p>';
+				}
+				
+				ob_end_flush();
+			?>
+
 		</center>
 	</body>
 </html>
 
-<?php
-	}
-	else {
-		echo "<center><h3>"."No Such Admission found"."</h3></center>";
-		echo '<p><center><input type="button" style="height:30px/;width:200px" value="Retry" onclick="window.location =\'enroll_name_del.php\'" /></p>';
-		echo "</br></br>";
-		echo '<p><center><input type="button" style="height:30px/;width:200px" value="Go to Home" onclick="window.location =\'dashboard.php\'" /></p>';
-	}
-	
-	ob_end_flush();
-?>
